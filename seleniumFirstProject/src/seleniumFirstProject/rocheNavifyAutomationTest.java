debcef570e6809234ad5bd04456c8e11eb41bd64
Alan West <awest0620@email.com>
2020-04-05 20:46:30 -0700

    Committing new java class file to repository.

---
+++ seleniumFirstProject/src/seleniumFirstProject/rocheNavifyAutomationTest.java
@@ -0,0 +1,32 @@
+package seleniumFirstProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class rocheNavifyAutomationTest {
+	
+	static WebDriver driver;
+
+	public void launchBrowser(){
+		System.setProperty("webdriver.chrome.driver", "/Users/alanwest/Desktop/Selenium WebDriver/Drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.navigate().to("https://www.google.com/");
+		
+	}
+	
+	public void closeBrowser() {
+		driver.close();
+	}
+
+
+	public static void main(String[] args) {
+		rocheNavifyAutomationTest obj = new rocheNavifyAutomationTest(); 
+        obj.launchBrowser();
+        obj.closeBrowser();
+        
+        
+
+	}
+
+}
